96acb3446a23a8fb213fb9e9ea96ebcf37881557
arzzuu <arzzuu>
2022-11-10 02:15:00 +0400

    changed

--- functional.js
+++ functional.js
@@ -1,4 +1,17 @@
-const element = document.querySelector('input .enter');
-element.addEventListener('mouseover', (event) => {
-    event.classList. toggle('.o') ;
-});
+const element = document.querySelector('.in');
+const secButt = document.querySelector('.knopka');
+// secButt.forEach((element) => {  
+   secButt.addEventListener('click', deleteValue);
+  
+// });
+
+element.addEventListener('click', getListContent);
+function getListContent()
+{
+   div.insertAdjacentHTML('afterend','<li><input type="text" id="enter"><button type="submit" class="knopka"><img src="./plus.svg" class="plusik"> </button></li>')
+}
+
+function deleteValue(){
+   document.getElementById('enter').value = "";
+   // document.getElementById('li').value = "";
+}
